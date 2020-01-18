e9c9376f0dab1e86733f24f34a2390fa1e02e024
Rafał Kotusiewicz <rk@buzzlers.com>
2020-01-18 18:13:36 +0100

    Fix formatting

--- src/ht/boot.clj
+++ src/ht/boot.clj
@@ -9,13 +9,14 @@
 (defn- create-app-directory-if-not-exists []
   (let [app-directory (io/file (str (System/getProperty "user.home") File/separator ".ht"))]
     (if-not (.exists app-directory)
-      (do (print (str "Creating app directory in " app-directory ".."))
-          (if (.mkdir app-directory)
-            (println ".created successful.")
-            (do
-              (println "Ups! Can't create app directory [" app-directory "]")
-              (System/exit -1))
-            )))))
+      (do
+        (print (str "Creating app directory in " app-directory ".."))
+        (if (.mkdir app-directory)
+          (println ".created successful.")
+          (do
+            (println "Ups! Can't create app directory [" app-directory "]")
+            (System/exit -1))
+          )))))
 
 (defn -main [& args]
   (print args)
