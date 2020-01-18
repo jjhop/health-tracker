ad5cb88e99164f7d6bb968ec0df4918a4e5cd973
Rafał Kotusiewicz <rk@buzzlers.com>
2020-01-18 17:47:30 +0100

    Add initial starting procedur (creating app directory in /.ht/ for database and config file.

--- src/ht/boot.clj
+++ src/ht/boot.clj
@@ -1,9 +1,28 @@
 (ns ht.boot
-  (:require [ht.gui :as g])
+  (:require [ht.gui :as g]
+            [clojure.java.io :as io])
+  (:import java.io.File)
   (:gen-class
-     :name ht.boot.Main))
+   :name ht.boot.Main))
+
+
+(defn- create-app-directory-if-not-exists []
+  (let [app-directory (io/file (str (System/getProperty "user.home") File/separator ".ht"))]
+    (if-not (.exists app-directory)
+      (do
+        (print (str "Creating app directory in " app-directory ".."))
+        (let [created? (.mkdir app-directory)]
+          (if created?
+            (println ".created successful.")
+            (do
+              (println "Ups! Can't create app directory [" app-directory "]")
+              (System/exit -1))
+      ))))))
 
 (defn -main [& args]
   (print args)
   (println "\nIt's a humble start to a great application!")
+  (create-app-directory-if-not-exists)
   (g/main-window))
+
+
