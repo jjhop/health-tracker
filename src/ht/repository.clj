864f40925da2117506a8138778b3720296926320
Rafał Kotusiewicz <rk@buzzlers.com>
2020-01-18 20:43:12 +0100

    Initial source for repository functions

--- src/ht/repository.clj
+++ src/ht/repository.clj
@@ -1,1 +1,46 @@
-(ns ht.repository)
+(ns ht.repository
+  (:require
+   [clojure.java.io :as io]
+   [clojure.java.jdbc :as jdbc]))
+
+
+(defn database-spec [app-data-directory]
+  {:classname   "org.hsqldb.jdbcDriver"
+   :subprotocol "hsqldb"
+   :subname (str "jdbc:hsqldb:file://" app-data-directory "/data.db")
+   :user     "SA"
+   :password ""})
+
+;;
+;; repository functions
+;;
+(defn blood-metrics-first-day []
+  ;; returns first day with blood metric
+  ;; as
+  ;; {
+  ;;  :day '2020-01-01'
+  ;;  :metric {
+  ;;     :id 1
+  ;;     :metric_date "2020-01-01"
+  ;;     :pressure "120 90"
+  ;;     :pulse 67
+  ;;  }
+  ;; }
+  ;; TODO: implementation
+  nil)
+
+(defn blood-metrics-between [start end]
+  ;; returns all blood metrics
+  ;; start - java.time.LocalDate -> first day of metrics to show
+  ;; end - java.time.LocalDate - last day of metrics to show
+
+  ;; TODO: implementation
+  nil)
+
+(defn weight-metrics-between [start end]
+  ;; returns weight metrics
+  ;; start - java.time.LocalDate -> first day of metrics to show
+  ;; end - java.time.LocalDate - last day of metrics to show
+
+  ;; TODO: implementation
+  nil)
