224d6bcc65d04b6ca5de005c5ffe7f359b4adaa9
Rafał Kotusiewicz <rk@buzzlers.com>
2020-01-18 17:54:00 +0100

    Add HyperSQL library as dependency. I chose hsqldb as database to store user data.

--- project.clj
+++ project.clj
@@ -3,6 +3,8 @@
   :url "https://github.com/jjhop/health-tracker"
   :license {:name "GNU General Public License v3.0"
             :url "https://github.com/jjhop/health-tracker/blob/master/LICENSE"}
-  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :dependencies [
+                 [org.clojure/clojure "1.10.1"]
+                 [org.hsqldb/hsqldb "2.5.0"]]
   :aot :all
   :main ht.boot.Main)
